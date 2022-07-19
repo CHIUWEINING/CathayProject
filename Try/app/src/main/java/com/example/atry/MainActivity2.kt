9417beb1f53f25b8ea92e18dcbb2eee488bf6394
koko <koko@kokos-MacBook-Pro.local>
2022-07-19 09:11:34 +0800

    Map

--- Try/app/src/main/java/com/example/atry/MainActivity2.kt
+++ Try/app/src/main/java/com/example/atry/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.atry
 
-import android.R
+
+import com.example.atry.R
 import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
@@ -10,19 +11,51 @@ import android.widget.CheckBox
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.atry.databinding.ActivityMain2Binding
+import com.example.atry.place.Place
+import com.example.atry.place.PlaceRenderer
+import com.example.atry.place.PlacesReader
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.maps.android.clustering.ClusterManager
 
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
-
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.black)
+        BitmapHelper.vectorToBitmap(this, R.drawable.list_icon, color)
+    }
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment)as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            //addMarkers(googleMap)
+            addClusteredMarkers(googleMap)
+
+            // Set custom info window adapter.
+            // googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+            googleMap.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+                places.forEach { bounds.include(it.latLng) }
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+            }
+        }
+
+
         var list= intent.getBundleExtra("list")?.getSerializable("list") as HashMap<String,String>
 
         fun expandCollapseSheet(){
@@ -122,6 +155,56 @@ class MainActivity2 : AppCompatActivity() {
                 }
             }).show()
 
+    }
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+                    //.icon(bicycleIcon)
+            )
+
+            // Set place as the tag on the marker object so it can be referenced within
+            // MarkerInfoWindowAdapter
+            marker?.tag = place
+        }
+    }
+    /**
+     * Adds markers to the map with clustering support.
+     */
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        // Create the ClusterManager class and set the custom renderer.
+        val clusterManager = ClusterManager<Place>(this, googleMap)
+        clusterManager.renderer =
+            PlaceRenderer(
+                this,
+                googleMap,
+                clusterManager
+            )
+
+        // Set custom info window adapter
+        clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+
+        // Add the places to the ClusterManager.
+        clusterManager.addItems(places)
+        clusterManager.cluster()
+
+        // Set ClusterManager as the OnCameraIdleListener so that it
+        // can re-cluster when zooming in and out.
+        googleMap.setOnCameraIdleListener {
+            // When the camera stops moving, change the alpha value back to opaque.
+            clusterManager.markerCollection.markers.forEach { it.alpha = 1.0f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 1.0f }
+
+            // Call clusterManager.onCameraIdle() when the camera stops moving so that reclustering
+            // can be performed when the camera stops moving.
+            clusterManager.onCameraIdle()
+        }
+        googleMap.setOnCameraMoveStartedListener {
+            clusterManager.markerCollection.markers.forEach { it.alpha = 0.3f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 0.3f }
+        }
     }
 
 
